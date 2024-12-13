0a89f10f85e537ac2883c2c7686954d4e6e31d19
adolfodev10 <adolfomanueldev10@gmail.com>
2024-12-13 07:16:00 +0000

    ✨feat: Clone of Responsive Youtube
    Finished of create the Youtube clone

---
+++ youtube-clone/js/script.js
@@ -0,0 +1,48 @@
+var sanduiche = document.querySelector(".menu-sanduiche");
+var sidebars = document.querySelectorAll(".sidebar");
+var videoAreas = document.querySelectorAll(".video-container");
+var filtros = document.querySelectorAll(".filtros");
+
+var inscricoes = document.querySelectorAll(".inscricoes");
+var explorar = document.querySelectorAll(".explorar");
+var maisYT = document.querySelectorAll(".mais-youtube");
+var config = document.querySelectorAll(".configuracoes");
+var final = document.querySelectorAll(".final");
+var linhas = document.querySelectorAll(".linha");
+var videos = document.querySelectorAll(".video");
+
+var outrosIcones = document.querySelector(".outros-icones");
+
+sanduiche.onclick = function () {
+  sidebars.forEach(function (sidebar) {
+    sidebar.classList.toggle("small-sidebar");
+  });
+  videoAreas.forEach(function (videoArea) {
+    videoArea.classList.toggle("aumenta-video-container");
+  });
+  filtros.forEach(function (filtro) {
+    filtro.classList.toggle("small-filtros");
+  });
+  inscricoes.forEach(function (inscricao) {
+    inscricao.classList.toggle("esconde-inscricoes");
+  });
+  explorar.forEach(function (itemExplorar) {
+    itemExplorar.classList.toggle("esconde-explorar");
+  });
+  maisYT.forEach(function (itemMaisYT) {
+    itemMaisYT.classList.toggle("esconde-mais-youtube");
+  });
+  config.forEach(function (itemConfig) {
+    itemConfig.classList.toggle("esconde-configuracoes");
+  });
+  final.forEach(function (itemFinal) {
+    itemFinal.classList.toggle("esconde-final");
+  });
+  linhas.forEach(function (linha) {
+    linha.classList.toggle("esconde-linha");
+  });
+  videos.forEach(function (video) {
+    video.classList.toggle("aumenta-video");
+  });
+  outrosIcones.classList.toggle("esconde-outros-icones");
+}
